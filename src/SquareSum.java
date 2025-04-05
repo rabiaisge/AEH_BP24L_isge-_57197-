4cdb14cbef944c53a13f64dcbc6c243132a054d9
Rabia Isge <rabiaisge5@example.com>
2025-04-05 13:11:43 +0200

    Updated MySecondApp.java to English version for Lab2 submission.

---
+++ src/SquareSum.java
@@ -0,0 +1,30 @@
+
+import java.util.Scanner;
+
+    public class SquareSum {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+
+            while (true) {
+                System.out.print("Enter lower and upper integer limits: ");
+                int lower = scanner.nextInt();
+                int upper = scanner.nextInt();
+
+                if (upper <= lower) {
+                    System.out.println("Done");
+                    break;
+                }
+
+                int sum = 0;
+                for (int i = lower; i <= upper; i++) {
+                    sum += i * i;
+                }
+
+                System.out.printf("The sums of the squares from %d to %d is %d%n", lower * lower, upper * upper, sum);
+            }
+
+            scanner.close();
+        }
+    }
+
+
