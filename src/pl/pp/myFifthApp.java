a25f6cfe06fd764d897922d04a5b98d3cb82ecdb
Rabia Isge <rabiaisge5@example.com>
2025-04-12 15:52:40 +0200

    Updated MySecondApp.java to English version for Lab2

---
+++ src/pl/pp/myFifthApp.java
@@ -0,0 +1,103 @@
+
+/*
+package pl.pp;
+
+public class myFifthApp {
+    public static void main(String[] args) {
+
+   // Calculations and display of the result for values assigned in the application code
+        System.out.println("Calculations and display of the result for values assigned in the application code");
+        boolean gameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+        int finalScore = score;
+        if (gameOver) {
+            finalScore += (levelCompleted * bonus);
+            finalScore += 1000;
+            System.out.println("Your score #1 is " + finalScore);
+        }
+
+        // Calculations and display of the result for values assigned in the application code (different than before)
+        System.out.println("Calculations and display of the result for values assigned in the application code (different than before)");
+        score = 10000;
+        levelCompleted = 8;
+        bonus = 200;
+
+        finalScore = score;
+
+        if (gameOver) {
+            finalScore += (levelCompleted * bonus);
+            finalScore += 1000;
+            System.out.println("Your score #2 is " + finalScore);
+        }
+
+        // Calculations using the method calculateScore_noArguments(), but still for values assigned in the method code
+        System.out.println("Calculations using the method calculateScore_noArguments(), but still for values assigned in the method code");
+        calculateScore_noArguments();
+
+        // Calculations using the method calculateScore(), but this time for values entered into the method call argument
+        System.out.println("Calculations using the method calculateScore(), but this time for values entered into the method call argument");
+        calculateScore_arguments(true, 2500, 9, 250);
+        // it can be called using previously declared variables
+        calculateScore_arguments(gameOver, score, levelCompleted, bonus);
+        // but as you see, the display function is also with number #4 (because such display is recorded
+        // in the method. What to do to avoid this? Below:
+
+        // Calculations and display can be made independent from each other by adding a parameter that this method will return using return
+        System.out.println("Calculations and display can be made independent from each other by adding a parameter that this method will return using return");
+        finalScore = calculateScore_argumentsReturn(gameOver, score, levelCompleted, bonus);
+        System.out.println("Your score #5 is " + finalScore);
+    }
+
+    private static void calculateScore_noArguments() {
+        boolean gameOver = true;
+        int score = 150;
+        int levelCompleted = 2;
+        int bonus = 1000;
+
+        int finalScore = score;
+        if (gameOver) {
+            finalScore += (levelCompleted * bonus);
+            finalScore += 1000;
+            System.out.println("Your score #3 is " + finalScore);
+        }
+    }
+    private static void calculateScore_arguments(boolean gameOver, int score, int levelCompleted, int bonus) {
+        int finalScore = score;
+        if (gameOver) {
+            finalScore += (levelCompleted * bonus);
+            finalScore += 1000;
+            System.out.println("Your score #4 is " + finalScore);
+        }
+    }
+    private static int calculateScore_argumentsReturn(boolean gameOver, int score, int levelCompleted, int bonus) {
+        int finalScore = score;
+        if (gameOver) {
+            finalScore += (levelCompleted * bonus);
+            finalScore += 1000;
+        }
+        return finalScore;
+    }
+}*/
+
+package pl.pp;
+
+public class myFifthApp {
+    public static void main(String[] args) {
+
+        // Task 2: Call the method that prints a box of characters
+        printCharacterBox('#', 10, 5); // Example: print '#' 10 times per line, for 5 lines
+    }
+
+    // Method: prints a character in a box shape based on width and height
+    private static void printCharacterBox(char character, int width, int height) {
+        for (int i = 0; i < height; i++) { // Loop through the number of lines
+            for (int j = 0; j < width; j++) { // Print the character multiple times per line
+                System.out.print(character);
+            }
+            System.out.println(); // Move to the next line after each row
+        }
+    }
+}
